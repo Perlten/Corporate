acaed391caaa770581bd05671a0c7a9d6687f4e5
adamlass <33089759+adamlass@users.noreply.github.com>
2018-09-07 13:13:43 +0200

    Corporate site

---
+++ Corporate/src/main/webapp/js/linkers.js
@@ -0,0 +1,11 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+var CA1 = document.getElementById("CA1");
+CA1.addEventListener("click",function (){
+    window.location.replace("/CA1");
+});
